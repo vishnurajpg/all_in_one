6e74b02ab9a3545fc827869f326ea092f9d70ffe
vishnurajpg <121508541+vishnurajpg@users.noreply.github.com>
2023-09-20 22:47:42 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>all in one</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10">
+              <div class="card" >
+                    <div class="card-head">
+                            <h1>Student Library</h1>
+                    </div>
+                     <div class="card-body">
+                            <button><a href="add.php"> New User </a></button>
+                            <table class="table">
+                            <thead>
+                                <tr>
+                                <th scope="col">Id</th>
+                                <th scope="col">User Name</th>
+                                <th scope="col">Password</th>
+                                <th scope="col">Location</th>
+                                <th scope="col">Contact</th>
+                                <th scope="col">Extra</th>
+
+                                </tr>
+                            </thead>
+                            <tbody>
+                                    <?php 
+                                        
+                                    ?>
+                            </tbody>
+</table>
+              </div>
+           </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
